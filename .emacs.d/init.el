c44f1b476f7d460b6d730796dc103ef161ee61c9
wat-aro <kazutas1008@gmail.com>
2016-10-10 10:00:01 +0900

    elixir-modeではsmartparensでendの補完をするように修正

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -718,14 +718,16 @@
 (require 'alchemist)
 (require 'flycheck-elixir)
 (add-to-list 'elixir-mode-hook
-             (defun auto-activate-ruby-end-mode-for-elixir-mode ()
-               (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
-                    "\\(?:^\\|\\s-+\\)\\(?:do\\)")
-               (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
-               (ruby-end-mode +1)
+             'ac-alchemist-setup)
 
-               (ac-alchemist-setup)
-               ))
+(sp-with-modes '(elixir-mode)
+  (sp-local-pair "fn" "end"
+         :when '(("SPC" "RET"))
+         :actions '(insert navigate))
+  (sp-local-pair "do" "end"
+         :when '(("SPC" "RET"))
+         :post-handlers '(sp-ruby-def-post-handler)
+         :actions '(insert navigate)))
 
 ;; ;;; flycheck OCaml
 ;; (with-eval-after-load 'merlin
