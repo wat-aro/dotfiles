95120ae644d83ec348192cccb7ea84d8c0b4ef36
wat-aro <kazutas1008@gmail.com>
2016-11-06 15:59:48 +0900

    sierraでの設定をアップデート

--- .emacs.d/inits/91-skk.el
+++ .emacs.d/inits/91-skk.el
@@ -5,11 +5,21 @@
   (define-key skk-j-mode-map ";" nil))
 (add-hook 'skk-mode-hook 'skk-mode-hook--unset-key)
 
+(setq skk-byte-compile-init-file t)
+
 (setq skk-user-directory "~/.emacs.d/ddskk/")
 (setq skk-tut-file "~/.emacs.d/share/skk/SKK.tut") ;;チュートリアルファイルの場所
+
 (require 'skk-autoloads)
+;;(require 'skk-dcomp)
+
+(setq default-input-method
+      "japanese-skk"			; (skk-mode 1)
+;;    "japanese-skk-auto-fill"		; (skk-auto-fill-mode 1)
+      )
+
 ;; key
-(define-key global-map (kbd "C-x C-o") 'skk-mode)
+(define-key global-map (kbd "C-c C-o") 'skk-mode)
 (setq skk-kakutei-key (kbd "C-o"))
 (setq mac-pass-control-to-system nil)
 
@@ -33,7 +43,7 @@
 
 ;; skk-latin-mode for all buffers
 (define-globalized-minor-mode global-skk-latin-mode skk-latin-mode skk-latin-mode-on)
-(global-skk-latin-mode 1)
+;;(global-skk-latin-mode 1)
 
 (setq skk-show-inline 'vertical)
 (skk-show-mode)
