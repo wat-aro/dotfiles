fae48557323faeed99b2fad258ea75f35c84a959
wat-aro <kazutas1008@gmail.com>
2017-02-19 16:41:22 +0900

    aquaskkをinstall.shで設定するのをやめる

--- install.sh
+++ install.sh
@@ -11,7 +11,3 @@ do
 done
 ln -snf ~/dotfiles/private.xml ~/Library/Application\ Support/Karabiner
 ln -snf ~/dotfiles/skk-jisyo.utf8 ~/Library/Application\ Support/AquaSKK
-ln -snf ~/dotfiles/my-custom.zsh ~/.oh-my-zsh/custom/
-ln -snf ~/dotfiles/my-custom.zsh ~/my-custom.zsh
-ln -snf ~/Dropbox/.ssh ~/.ssh
-ln -snf ~/dotfiles/keymap.conf ~/Library/Application\ Support/AquaSKK
