2eccd167a2b4017414ac2e04d770d081124085fa
wat-aro <kazutas1008@gmail.com>
2017-02-20 21:08:02 +0900

    flycheck-list-errorsのキーバインドが効いていなかったので修正

--- .emacs.d/inits/07-flycheck.el
+++ .emacs.d/inits/07-flycheck.el
@@ -1,13 +1,12 @@
 ;; Flycheck
 (use-package flycheck
   :ensure t
+  :bind (("M-g l" . flycheck-list-errors))
   :config
   (custom-set-variables
    '(flycheck-display-errors-delay 0.5)
    '(flycheck-idle-change-delay 1.0)
-   '(flycheck-display-errors-function nil))
-  (bind-keys :map flycheck-command-map
-             ("M-g l" . flycheck-list-errors)))
+   '(flycheck-display-errors-function nil)))
 
 (with-eval-after-load 'flycheck
   (flycheck-pos-tip-mode))
