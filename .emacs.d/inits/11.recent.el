1fddce205dbfa23ec7f56aeba55c40f29f3235eb
wat-aro <kazutas1008@gmail.com>
2017-02-25 22:21:34 +0900

    recentfが自動で削除しないようにする

--- .emacs.d/inits/11.recent.el
+++ .emacs.d/inits/11.recent.el
@@ -3,7 +3,7 @@
   :init
   (custom-set-variables
    '(recentf-max-saved-items 2000)
-   '(recentf-auto-cleanup 600)
+   '(recentf-auto-cleanup 'never)
    '(recentf-exclude '("/recentf" "/elpa/" "/elisps/" "\\'/tmp/" "/\\.git/" "/\\.cask/"
                        "COMMIT_EDITMSG" "/.?TAGS" "^/sudo:" "/\\.emacs\\.d/games/*-scores"))
    (list 'recentf-auto-save-timer (run-with-idle-timer 30 t 'recentf-save-list)))
