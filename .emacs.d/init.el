00c5a24e47e8a1d3b64da0514c7a8d8c5b2ad45b
wat-aro <kazutas1008@gmail.com>
2017-02-26 00:14:28 +0900

    末尾の空白文字の可視化

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -85,9 +85,3 @@
      (360 . "#dc8cc3"))))
  '(vc-annotate-very-old-color "#dc8cc3")
  '(view-read-only t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- .emacs.d/inits/05-space.el
+++ .emacs.d/inits/05-space.el
@@ -1,2 +1,23 @@
 ;; White space
-;; (add-hook 'before-save-hook 'delete-trailing-whitespace)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(require 'whitespace)
+(setq whitespace-style '(face           ; faceで可視化
+                         trailing       ; 行末
+                         tabs           ; タブ
+;;                         empty          ; 先頭/末尾の空行
+                         space-mark     ; 表示のマッピング
+                         tab-mark
+                         ))
+
+(setq whitespace-display-mappings
+      '((tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+
+(global-whitespace-mode 1)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-trailing ((t (:foreground "magenta" :underline t :background nil)))))
