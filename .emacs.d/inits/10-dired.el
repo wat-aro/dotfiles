ade4ef5fab1e8583677174c4b1a3d2d5a0c546af
wat-aro <kazutas1008@gmail.com>
2017-02-26 00:38:23 +0900

    recursive copy in dired mode

--- .emacs.d/inits/10-dired.el
+++ .emacs.d/inits/10-dired.el
@@ -1,6 +1,10 @@
 ;; direx
 (use-package direx :defer t
   :init
+  ;; diredを2つのウィンドウで開いている時に、デフォルトの移動orコピー先をもう一方のdiredで開いているディレクトリにする
+  (setq dired-dwim-target t)
+  ;; ディレクトリを再帰的にコピーする
+  (setq dired-recursive-copies 'always)
   (bind-key "M-C-\\" 'direx-project:jump-to-project-root-other-window))
 
 ;; dired-k
