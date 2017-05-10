863d51d1514737a26266854f6eea64a854215f53
wat-aro <kazutas1008@gmail.com>
2017-05-10 09:00:25 +0900

    Add coq-mode

--- .emacs.d/inits/00-base.el
+++ .emacs.d/inits/00-base.el
@@ -2,29 +2,17 @@
 (setq-default gc-cons-percentage 0.5)
 
 ;; PATH
-;; fish
-
-;; (let ((path-from-shell (shell-command-to-string "/usr/local/bin/fish -i -c \"echo -n \\$PATH[1]; for val in \\$PATH[2..-1];echo -n \\\":\\$val\\\";end\"")))
-;;   (setenv "PATH" path-from-shell)
-;;   (setq exec-path (split-string path-from-shell ":")))
-
-; zsh
-;; (when (memq window-system '(mac ns))
-;;   (exec-path-from-shell-initialize))
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
 
 (let ((default-directory (locate-user-emacs-file "./elisp")))
   (add-to-list 'load-path default-directory)
   (normal-top-level-add-subdirs-to-load-path))
 
-(load-file (expand-file-name "~/.emacs.d/shellenv.el"))
-(dolist (path (reverse (split-string (getenv "PATH") ":")))
-  (add-to-list 'exec-path path))
-;; (let ((opam-directory "~/.opam/system/share/emacs/site-lisp")
-;;       (rbenv-directory "~/.rbenv/shims")
-;;       (local-path "~/.local/bin"))
-;;   (add-to-list 'exec-path opam-directory)
-;;   (add-to-list 'exec-path rbenv-directory)
-;;   (add-to-list 'exec-path local-path))
+(let ((opam-directory "~/.opam/system/share/emacs/site-lisp")
+      (rbenv-directory "~/.rbenv/shims"))
+  (add-to-list 'load-path opam-directory)
+  (add-to-list 'load-path rbenv-directory))
 
 ;;; Language
 (set-language-environment 'Japanese)
---
+++ .emacs.d/inits/29-coq.el
@@ -0,0 +1,1 @@
+(load-file "/usr/local/share/emacs/site-lisp/proof-general/generic/proof-site.el")
