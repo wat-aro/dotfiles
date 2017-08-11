801d80a5f7e988363cd3a66a28f525745556a3db
wat-aro <kazutas1008@gmail.com>
2017-08-11 16:22:07 +0900

    Add clojure

---
+++ .emacs.d/inits/34-clojure.el
@@ -0,0 +1,38 @@
+(use-package clojure-mode
+  :init
+  (add-hook 'clojure-mode-hook #'paredit-mode)
+  (custom-set-variables
+   '(clojure-align-forms-automatically t)))
+
+(use-package cider
+  :init
+  (add-hook 'cider-mode-hook #'clj-refactor-mode)
+  (add-hook 'cider-mode-hook #'company-mode)
+  (add-hook 'cider-mode-hook #'eldoc-mode)
+  (add-hook 'cider-repl-mode-hook #'company-mode)
+  (add-hook 'cider-repl-mode-hook #'eldoc-mode)
+  :diminish subword-mode
+  :config
+  (setq nrepl-log-messages t
+        cider-repl-display-in-current-window t
+        cider-repl-use-clojure-font-lock t
+        cider-prompt-save-file-on-load 'always-save
+        cider-font-lock-dynamically '(macro core function var)
+        cider-overlays-use-font-lock t)
+  (cider-repl-toggle-pretty-printing))
+
+(use-package company
+  :config
+  (setq company-idle-delay 0.1
+        company-minimum-prefix-length 2
+        company-selection-wrap-around t)
+
+  (bind-keys :map company-mode-map
+             ("C-i" . company-complete))
+  (bind-keys :map company-active-map
+             ("C-n" . company-select-next)
+             ("C-p" . company-select-previous)
+             ("C-s" . company-search-words-regexp))
+  (bind-keys :map company-search-map
+             ("C-n" . company-select-next)
+             ("C-p" . company-select-previous)))
