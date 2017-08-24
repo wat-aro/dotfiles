c65bc332dca43dc0075d7b858628546222cbb6dc
wat-aro <kazutas1008@gmail.com>
2017-08-25 01:59:22 +0900

    (anzu-use-migemo t)

--- .emacs.d/inits/03-search.el
+++ .emacs.d/inits/03-search.el
@@ -8,7 +8,8 @@
    (quote
     ("bdaab014ec6785f64b72efbea80808b762d8971247aacf2ffc6b76a39b9ed97c" default)))
  '(rspec-use-rake-when-possible nil)
- '(tab-width 2))
+ '(tab-width 2)
+ '(anzu-use-migemo t))
 
 (use-package isearch
   :bind
