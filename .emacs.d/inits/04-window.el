16fc38bcf28886c869a615c8758bdfda8f4a9e19
wat-aro <kazutas1008@gmail.com>
2017-08-24 23:44:07 +0900

    popupの高さを変える

--- .emacs.d/inits/04-window.el
+++ .emacs.d/inits/04-window.el
@@ -4,6 +4,7 @@
   :config
   (when (fboundp 'popwin:w3m-browse-url)
     (setq browse-url-browser-function 'popwin:w3m-browse-url))
+  (setq popwin:popup-window-height 0.4)
   (setq popwin:special-display-config
         (append popwin:special-display-config
                 '(("*w3m*")
@@ -16,7 +17,7 @@
                   (sldb-mode :height 40 :stick t)
                   slime-repl-mode
                   slime-connection-list-mode
-                  (direx:direx-mode :position left :width 25 :dedicated t)
+                  (direx:direx-mode :position left :width 40 :dedicated t)
                   ("*quickrun*")
                   (dired-mode :position top)
                   ("*Shell Command Output*")
