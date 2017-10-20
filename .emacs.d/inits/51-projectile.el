0766f6aeb6caad638472ceaf2a8b60ef9bffed27
wat-aro <kazutas1008@gmail.com>
2017-10-20 12:05:12 +0900

    Fix projectile setting
    重くなったので設定を追加
    cf: https://github.com/bbatsov/projectile/issues/1183

--- .emacs.d/inits/51-projectile.el
+++ .emacs.d/inits/51-projectile.el
@@ -3,7 +3,10 @@
   :config
   (use-package helm-projectile)
   (custom-set-variables
-   '(projectile-completion-system 'helm))
+   '(projectile-completion-system 'helm)
+   '(projectile-mode-line
+     '(:eval (format " Projectile[%s]"
+                     (projectile-project-name)))))
   (projectile-global-mode t)
   (helm-projectile-on))
 
