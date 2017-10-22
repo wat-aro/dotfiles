6993a237565369979bce3ebf0471c7efee5f1114
wat-aro <kazutas1008@gmail.com>
2017-10-23 01:08:14 +0900

    update haskell mode

--- .emacs.d/inits/28-haskell.el
+++ .emacs.d/inits/28-haskell.el
@@ -1,37 +1,13 @@
-;; (add-hook 'haskell-mode-hook 'hindent-mode)
-
-;; (eval-after-load 'flycheck
-;;   '(add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
-
-;; (require 'ac-haskell-process) ; if not installed via package.el
-;; (add-hook 'interactive-haskell-mode-hook 'ac-haskell-process-setup)
-;; (add-hook 'haskell-interactive-mode-hook 'ac-haskell-process-setup)
-;; (add-hook 'inferior-haskell-mode-hook 'ac-haskell-process-setup)
-;; (eval-after-load "auto-complete"
-;;   '(add-to-list 'ac-modes 'haskell-interactive-mode))
-
-;; (defun set-auto-complete-as-completion-at-point-function ()
-;;   (add-to-list 'completion-at-point-functions 'auto-complete))
-;; (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
-;; (add-to-list 'ac-modes 'haskell-interactive-mode)
-;; (add-hook 'haskell-interactive-mode-hook 'set-auto-complete-as-completion-at-point-function)
-;; (add-hook 'haskell-mode-hook 'set-auto-complete-as-completion-at-point-function)
-
-;; (eval-after-load 'haskell-mode
-;;   '(define-key haskell-mode-map (kbd "C-c C-d") 'ac-haskell-process-popup-doc))
-
-;; (add-hook 'haskell-mode-hook
-;;           '(lambda ()
-;;              (setq flycheck-checker 'haskell-hlint)
-;;              (flycheck-mode 1)))
 (use-package haskell-mode
   :init
-  (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
-  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-  (add-hook 'haskell-mode-hook 'haskell-decl-scan-mode)
-  (add-hook 'haskell-mode-hook 'haskell-doc-mode)
-  (add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
-  (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+  (add-hook 'haskell-mode-hook (lambda ()
+                                 (haskell-indentation-mode)
+                                 (interactive-haskell-mode)
+                                 (haskell-decl-scan-mode)
+                                 (haskell-doc-mode)
+                                 (haskell-auto-insert-module-template)
+                                 (ghc-init)
+                                 (flycheck-mode t)))
   (custom-set-variables '(haskell-compile-cabal-build-command "stack build")
                         '(haskell-process-type 'stack-ghci)
                         '(haskell-process-path-ghci "stack")
@@ -45,9 +21,9 @@
               ("C-c C-l" . haskell-process-load-or-reload)
               ("C-j"     . haskell-indentation-newline-and-indent)
               ("C-c C-?" . haskell-hoogle)
-              ("C-c C-c" . haskell-compile))
-  :config
-  (bind-keys :map haskell-indentation-mode-map
+              ("C-c C-c" . haskell-compile)
+              ("C-c C-d" . ac-haskell-process-popup-doc)
+              :map haskell-indentation-mode-map
              (";" . nil)))
 
 (use-package haskell-interactive-mode
@@ -60,3 +36,15 @@
   ("\\.cabal\\'" . haskell-cabal-mode)
   :bind (:map haskell-cabal-mode-map
               ("C-c C-c" . haskell-compile)))
+
+(defun set-auto-complete-as-completion-at-point-function ()
+  (add-to-list 'completion-at-point-functions 'auto-complete))
+
+(use-package ac-haskell-process
+  :init
+  (add-hook 'interactive-haskell-mode-hook 'ac-haskell-process-setup)
+  (add-hook 'haskell-interactive-mode-hook 'ac-haskell-process-setup)
+  (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+  (add-to-list 'ac-modes 'haskell-interactive-mode)
+  (add-hook 'haskell-interactive-mode-hook 'set-auto-complete-as-completion-at-point-function)
+  (add-hook 'haskell-mode-hook 'set-auto-complete-as-completion-at-point-function))
