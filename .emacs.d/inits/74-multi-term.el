226e29da818c6df229beaf98079d07326f05f2ba
wat-aro <kazutas1008@gmail.com>
2018-02-15 01:32:40 +0900

    fix term-mode

--- .emacs.d/inits/74-multi-term.el
+++ .emacs.d/inits/74-multi-term.el
@@ -1,28 +1,32 @@
-  ;; multi-term
-  ;; emacs に認識させたいキーがある場合は、term-unbind-key-list に追加する
-  (require 'multi-term)
-  (dolist (x '("C-q"))
-    (add-to-list 'term-unbind-key-list x))
-  ;; terminal に直接通したいキーがある場合は、term-unbind-key-listからdeleteする
-  ;(dolist (x '("C-c"))
-  ;  (delete x term-unbind-key-list))
-  ;; term-mode-hook
-  (add-hook
-   'term-mode-hook
-   '(lambda ()
-      (define-key term-raw-map "\C-h" 'term-send-backspace)
-      (define-key term-raw-map "\C-y" 'term-paste)
-      (define-key term-raw-map "\C-t" 'other-window)
-      (define-key term-raw-map term-view-toggle-key 'toggle-term-view)))
-  ;; C-t で term-mode と fundamental-mode + view-modeを切り替える
-  (setq term-view-toggle-key (kbd "C-q"))
-  (defun toggle-term-view () (interactive)
-         (cond ((eq major-mode 'term-mode)
-                (fundamental-mode)
-                (view-mode)
-                (local-set-key term-view-toggle-key 'toggle-term-view)
-                (setq multi-term-cursor-point (point)))
-               ((eq major-mode 'fundamental-mode)
-                (View-exit)
-                (goto-char multi-term-cursor-point)
-                (multi-term-internal))))
+(require 'multi-term)
+
+(defun toggle-term-mode()
+  "Toggles term mode(line <--> char)"
+  (interactive)
+  (if (term-in-line-mode)
+      (term-char-mode)
+    (term-line-mode)))
+
+(setq multi-term-program shell-file-name)
+
+(global-set-key "\C-c\C-t" 'multi-term)
+
+;; keybinds for multi-term
+(defun term-mode-hooks ()
+  ;term-char
+  (define-key term-raw-map (kbd "C-c C-p") 'multi-term-prev)
+  (define-key term-raw-map (kbd "C-c C-n") 'multi-term-next)
+  (define-key term-raw-map (kbd "C-c C-j") 'toggle-term-mode)
+  (define-key term-raw-map (kbd "C-t") 'other-window)
+
+  (define-key term-raw-map (kbd "C-p") 'term-send-up)
+  (define-key term-raw-map (kbd "C-n") 'term-send-down)
+
+  ;term-line
+  (define-key term-mode-map (kbd "C-c C-p") 'multi-term-prev)
+  (define-key term-mode-map (kbd "C-c C-n") 'multi-term-next)
+  (define-key term-mode-map (kbd "C-c C-j") 'toggle-term-mode)
+  (define-key term-mode-map (kbd "C-t") 'other-window))
+
+
+(add-hook 'term-mode-hook 'term-mode-hooks)
