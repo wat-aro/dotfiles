b98f84aab275b2b46c93f423e1545d474007627b
wat-aro <kazutas1008@gmail.com>
2018-05-19 02:39:50 +0900

    elm の tag を自動生成

--- .emacs.d/inits/32-elm.el
+++ .emacs.d/inits/32-elm.el
@@ -1,6 +1,7 @@
 (use-package elm-mode
   :init
-  (custom-set-variables '(elm-indent-offset 4))
+  (custom-set-variables '(elm-indent-offset 4)
+                        '(elm-tags-on-save t))
   (add-hook 'elm-mode-hook #'elm-oracle-setup-completion)
   (add-to-list 'company-backends 'company-elm)
   (add-hook 'elm-mode-hook
