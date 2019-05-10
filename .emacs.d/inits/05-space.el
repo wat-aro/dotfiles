a4cf4d0350621f9eacc95a08fc8d7a40dd8acf25
wat-aro <kazutas1008@gmail.com>
2019-05-11 00:35:07 +0900

    Add smart-hungry-delete

--- .emacs.d/inits/05-space.el
+++ .emacs.d/inits/05-space.el
@@ -7,3 +7,11 @@
   (whitespace-trailing ((t (:foreground "magenta" :underline t :background nil))))
   :hook
   (before-save . delete-trailing-whitespace))
+
+(use-package smart-hungry-delete
+  :ensure t
+  :bind (("<DEL>" . smart-hungry-delete-backward-char)
+		     ("C-d" . smart-hungry-delete-forward-char))
+  :defer nil ;; dont defer so we can add our functions to hooks
+  :config (smart-hungry-delete-add-default-hooks)
+  )
