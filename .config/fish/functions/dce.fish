f81bd3278fa5057f39877ec86709f138298d02b4
wat-aro <kazutas1008@gmail.com>
2019-06-07 13:46:31 +0900

    Use abbr on docker command

--- .config/fish/functions/dce.fish
+++
@@ -1,3 +0,0 @@
-function dce --description 'Execute `docker-compose exec CONTAINER`'
-    docker-compose exec $APP_NAME $argv
-end
--- .config/fish/functions/dcr.fish
+++
@@ -1,3 +0,0 @@
-function dcr --description 'Execute `docker-compose run --rm APP_NAME`'
-    docker-compose run --rm $APP_NAME $argv
-end
