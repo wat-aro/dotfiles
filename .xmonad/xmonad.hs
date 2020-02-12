e387543fc2308bd85e85b45ee9fa82bc5f05c2b9
wat-aro <kazutas1008@gmail.com>
2020-02-12 16:44:54 +0900

    Update xmonad setting

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,10 +9,22 @@ myConfig = desktopConfig
   { terminal = myTerminal
   , modMask = mod4Mask -- Rebind Mod to the Windows key
   , startupHook = myStartupHook
+  , borderWidth = 3
+  , normalBorderColor = myNormalBorderColor
+  , focusedBorderColor = myFocusedBorderColor
+  , focusFollowsMouse = myFocusFollowsMouse
+  , workspaces = myWorkspaces
   }
 
 myTerminal = "urxvt"
+myFocusFollowsMouse = False
+
+myNormalBorderColor  = "#000000"
+myFocusedBorderColor = "#dddddd"
+
+myWorkspaces = ["terminal", "chat", "web", "emacs", "5", "6", "7", "8", "9"]
+
 myStartupHook = do
-  spawn "~/.screenlayout/private3.sh"
-  spawn "feh --bg-scale ~/Pictures/Wallpapers/arch-linux.jpg"
+  spawn "~/.screenlayout/office3.sh"
+  spawn "feh --bg-scale ~/Pictures/Wallpapers/arch-linux.png"
   spawn "albert"
