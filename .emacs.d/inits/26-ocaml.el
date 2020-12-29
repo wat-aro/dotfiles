a9e1fe835dfdae30374c760942abb90e9d0909e9
wat-aro <kazutas1008@gmail.com>
2020-12-29 13:10:14 +0900

    Fix ocaml settings

--- .emacs.d/inits/26-ocaml.el
+++ .emacs.d/inits/26-ocaml.el
@@ -1,21 +1,19 @@
 ;;; Ocaml
-(require 'opam-user-setup "~/.emacs.d/elisp/opam-user-setup.el")
-
 ;;; tuareg
-(add-to-list 'auto-mode-alist '("\\.ml[iylp]?" . tuareg-mode))
-(autoload 'tuareg-mode "tuareg" "Major mode for editing OCaml code" t)
-(autoload 'tuareg-run-ocaml "tuareg" "Run an inferior OCaml process." t)
-(autoload 'ocamldebug "ocamldebug" "Run the OCaml debugger" t)
-(setq tuareg-use-smie nil)
+(use-package tuareg
+  :mode (("\\.ml[iylp]?" . tuareg-mode))
+  :commands (tuareg-mode tuareg-run-ocaml ocamldebug)
+  :config
+  (setq tuareg-use-smie nil))
+
+;; (autoload 'ocamldebug "ocamldebug" "Run the OCaml debugger" t)
 
 (use-package merlin
   :init
   (setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
   :hook
   ((tuareg-mode caml-mode) . merlin-mode)
-  ;; (merlin-mode . (lambda ()
-  ;;                  (setq ac-sources (append ac-sources '(merlin-ac-source)))))
   :config
   (setq merlin-command 'opam)
   (setq merlin-error-after-save nil)
-  (flycheck-ocaml-setup))
+  (require 'opam-user-setup "~/.emacs.d/elisp/opam-user-setup.el"))
