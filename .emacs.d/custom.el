b238a1cc910573990bcd7f1ba794e78795542095
wat-aro <kazutas1008@gmail.com>
2020-12-29 11:09:39 +0900

    Extract customize variables to custom file

---
+++ .emacs.d/custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(enable-remote-dir-locals t)
+ '(parens-require-spaces nil)
+ '(recentf-auto-save-timer [nil 0 30 0 t recentf-save-list nil idle 0])
+ '(show-paren-delay 0)
+ '(uniquify-buffer-name-style 'post-forward-angle-brackets nil (uniquify))
+ '(view-read-only t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-bar ((t (:background "#6272a4"))))
+ '(linum ((t (:inherit (shadow default) :foreground "Gray"))))
+ '(whitespace-style (face trailing tabs space-mark tab-mark))
+ '(whitespace-trailing ((t (:foreground "magenta" :underline t :background nil)))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,44 +23,9 @@
 
 (use-package init-loader
   :init
-  (init-loader-load "~/.emacs.d/inits")
+  (init-loader-load (locate-user-emacs-file "inits"))
   :custom
-  (init-loader-show-log-after-init 'error-only))
+  (init-loader-show-log-after-init 'error-only)
+  (custom-file (locate-user-emacs-file "custom.el")))
 
 (server-start)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(anzu-deactivate-region t)
- '(anzu-mode-lighter "")
- '(anzu-search-threshold 1000)
- '(anzu-use-migemo t)
- '(doom-themes-enable-bold t)
- '(doom-themes-enable-italic t)
- '(init-loader-show-log-after-init (quote error-only))
- '(ivy-ghq-short-list t)
- '(ivy-height 20)
- '(parens-require-spaces nil)
- '(popwin:popup-window-height 0.4)
- '(recentf-auto-save-timer [nil 0 30 0 t recentf-save-list nil idle 0])
- '(rspec-use-rake-when-possible nil)
- '(safe-local-variable-values
-   (quote
-    ((rspec-docker-container . "spring")
-     (rspec-docker-cwd . "/usr/src/app/")
-     (rspec-use-docker-when-possible . t)
-     (rspec-docker-command . "docker-compose run --rm"))))
- '(show-paren-delay 0)
- '(tab-width 2)
- '(uniquify-buffer-name-style (quote post-forward-angle-brackets) nil (uniquify))
- '(view-read-only t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(doom-modeline-bar ((t (:background "#6272a4"))))
- '(whitespace-style (face trailing tabs space-mark tab-mark))
- '(whitespace-trailing ((t (:foreground "magenta" :underline t :background nil)))))
