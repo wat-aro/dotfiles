382982a4bca74347cf964d19a40e18be16dde3ef
wat-aro <kazutas1008@gmail.com>
2022-08-14 23:45:55 +0900

    Update

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,10 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(enable-remote-dir-locals t)
  '(js-indent-level 2)
+ '(package-selected-packages
+    '(paperless ddskk nyan-mode beacon which-key visual-regexp undo-tree editorconfig multiple-cursors open-junk-file docker-compose-mode dockerfile-mode docker projectile magit org-bullets cargo rust-mode cider sass-mode haml-mode slim-mode web-mode haskell-mode go-mode yaml-mode rubocop rinari projectile-rails rspec-mode inf-ruby lsp-ui lsp-mode yasnippet js2-mode dired-k paredit smartparens flycheck-posframe flycheck company-box company smart-hungry-delete doom-modeline neotree doom-themes migemo anzu ivy-rich all-the-icons-ivy-rich counsel-tramp swiper counsel ivy-hydra ivy sudo-edit s ace-window bind-key ht blackout el-get hydra leaf-keywords))
  '(parens-require-spaces nil)
- '(safe-local-variable-values
-    '((cargo-process--custom-path-to-bin . "/usr/local/cargo/bin/cargo")
-       (rspec-docker-command . "docker-compose run --rm")))
  '(show-paren-delay 0)
  '(uniquify-buffer-name-style 'post-forward-angle-brackets nil (uniquify)))
 (custom-set-faces
